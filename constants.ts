d6e19881c874149e561e1107088c761b765ce9db
Aleksei <saratkin.aleksey@gmail.com>
2022-06-27 13:16:36 +0300

    First commit

---
+++ constants.ts
@@ -0,0 +1,26 @@
+export const Mathematics: string = "Mathematics";
+export const Hebrew: string = "Hebrew";
+export const Geography: string = "Geography";
+// History constant can't be used in other files because it has no "export" keyword, so it is not exposed to other files
+// const History: string = "History";
+// To expose History to another files we add "export" keyword
+export const History: string = "History"; 
+export const Chemistry: string = "Chemistry";
+export const Physics: string = "Physics";
+
+export const firstNames: string[] = [
+    "Ivan",
+    "Peter",
+    "Olga",
+    "Fyodor",
+    "Oleg",
+    "Ksenia"
+];
+
+export const lastNames: string[] = [
+    "Petrov",
+    "Ivanov",
+    "Sidorov",
+    "Kukushkin",
+    "Fyodorov"
+];
