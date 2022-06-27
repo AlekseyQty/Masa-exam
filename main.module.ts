349105a576abd3655650e755aac51218ab57bd26
Aleksei <saratkin.aleksey@gmail.com>
2022-06-27 16:30:40 +0300

    Task 7 done

--- main.module.ts
+++ main.module.ts
@@ -2,18 +2,23 @@ import { Classroom } from "./classroom.service";
 import { firstNames, Geography, lastNames, Physics } from "./constants";
 import { getRandomBirthDate, getRandomValueFromArray } from "./helpers";
 import { School } from "./school.service";
+import { transferStudent } from "./services";
 import { Student } from "./student.service";
 import { Teacher } from "./teacher.service";
 
 export function initialize(): void {
     const teacherNew1: Teacher = new Teacher(getRandomValueFromArray(firstNames), getRandomValueFromArray(lastNames), [Geography, Physics]);
+    const teacherNew2: Teacher = new Teacher(getRandomValueFromArray(firstNames), getRandomValueFromArray(lastNames), [Geography, Physics]);
     const studentNew1: Student = new Student("Alex", "Bolt", getRandomBirthDate());
     const studentNew2: Student = new Student(getRandomValueFromArray(firstNames), getRandomValueFromArray(lastNames), getRandomBirthDate());
+    const studentNew3: Student = new Student(getRandomValueFromArray(firstNames), getRandomValueFromArray(lastNames), getRandomBirthDate());
     const newMathClass: Classroom = new Classroom("Math", teacherNew1, [studentNew1, studentNew2]);
-    const newSchool: School = new School("Small school", "New York", "+79813332244", [newMathClass]);
+    const newHistoryClass: Classroom = new Classroom("History", teacherNew2, [studentNew3]);
+    const newSchool: School = new School("Small school", "New York", "+79813332244", [newMathClass, newHistoryClass]);
     console.log(newMathClass.getClassYoungestStudent());
     // console.log(teacherNew1.fullName());
     newSchool.printSchool();
-    // newSchool.transferStudent("Alex Bolt", newMathClass, newMathClass);
+    transferStudent("Alex Bolt", newMathClass, newHistoryClass);
+    newSchool.printSchool();
     // console.log(newMathClass);
 }
--- main.ts
+++ main.ts
@@ -1,8 +1,8 @@
-import { School } from "./entities";
-import { getClassYoungestStudent as getClassYoungestStudentFullName, initializeSchool, printSchool } from "./services";
+// import { School } from "./entities";
+// import { getClassYoungestStudent as getClassYoungestStudentFullName, initializeSchool, printSchool } from "./services";
 
-const school: School = initializeSchool();
+// const school: School = initializeSchool();
 
-printSchool(school);
+// printSchool(school);
 
-console.log(getClassYoungestStudentFullName(school.classes[0]));
+// console.log(getClassYoungestStudentFullName(school.classes[0]));
